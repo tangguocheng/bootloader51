f5fb30cd5da25298f75c4fb3f84e9f81e2199941
guocheng tang <guocheng tang>
2016-07-24 16:15:33 +0800

    origin version

---
+++ bootloader.c
@@ -0,0 +1,635 @@
+#include "bootloader.h"
+extern void FV_UARTISR(void);
+// 全局变量定义
+//unsigned char xdata BTL_tempBuff[D_SECTORSIZE];
+unsigned char xdata currentSector = 0;
+//unsigned char xdata bootAddr[3];                                      // 用户更新用户程序时保存bootloader跳转指令及地址，防止bootloader入口被破坏
+unsigned char xdata bootFlag[2] _at_ D_FLAGADDR;                            // 升级标志
+/*************************************************************************
+*函数功能：bootloader主函数入口
+*参数：无
+*返回值：无
+*************************************************************************/
+void BTL_main(void)
+{
+    D_DISINT();
+    SPCFNC = 1;
+    XBYTE[0x0402] = 0x86;
+    SPCFNC = 0;
+    D_CLRWDT();
+    if (!BTL_setSysClock(4))                                            // 设置系统时钟13M
+    {
+        while (1);
+    }                                                                   // 关闭全局中断
+    BTL_uartInit();                                                     // 初始化串口、定时器、flash。因为无法使用中断服务，所有的通讯、定时由查询完成
+    BTL_timerInit();
+
+    BTL_sendEcho(D_BTL);                                                // 发送bootloader版本号等信息
+    while (1)
+    {
+        D_CLRWDT();
+        if (BTL_checkStatus())                                          // 判断是否处在升级状态
+        {
+            BTL_Updata();
+        }
+        else
+        {
+            D_CLRWDT();
+            #pragma ASM
+            EXTRN CODE (?C_START)
+            LJMP    ?C_START
+            #pragma ENDASM
+        }
+    }
+}
+/*************************************************************************
+*函数功能：8#中断服务函数
+*参数：无
+*返回值：无
+*************************************************************************/
+void UARTCF1_ISR(void) interrupt 8                                      // UARTANDCF1INT_VECTOR
+{
+    if (EXIF & BIT4)
+    {
+        while ((ExInt2IFG & 0xFF) != 0)
+        {
+            if (ExInt2IFG & BIT6)                                       // FLASH擦写中断
+            {
+                // FLASH擦写中断
+                ExInt2IFG &= (~BIT6);
+            }
+            else                                                        // 跳转到其他中断处理函数中
+            {
+                FV_UARTISR();
+            }
+        }
+        EXIF &= (~BIT4);
+    }
+}
+
+/*************************************************************************
+*函数功能：内存拷贝函数
+*参数：des，目的地址，src，源地址，len，数据长度
+*返回值：viod
+*************************************************************************/
+void BTL_memCpy(unsigned char *des, unsigned char const * src, unsigned int len)
+{
+    unsigned int i;
+    for (i = 0; i < len; i++)
+    {
+        (*des) = (*src);
+        des++;
+        src++;
+    }
+}
+/*************************************************************************
+*函数功能：内存清零函数
+*参数：des，目的地址，len，数据长度
+*返回值：无
+*************************************************************************/
+void BTL_memClr(unsigned char *des, unsigned int len)
+{
+    unsigned int idata i;
+    for (i = 0; i < len; i++)
+    {
+        (*des) = 0xFF;
+        des++;
+    }
+
+}
+/*************************************************************************
+功能: 页擦除
+参数: page,一页512字节
+返回: void
+***************************************************************************/
+void BTL_pageerase0(unsigned int page)
+{
+    unsigned char xdata * p;
+    SPC_FNC = 0x01;
+    p = (unsigned char xdata*)0x0555;
+    *p = 0xaa;
+    p = (unsigned char xdata*)0x02aa;
+    *p = 0x55;
+    p = (unsigned char xdata*)0x0555;
+    *p = 0x80;
+    p = (unsigned char xdata*)0x0555;
+    *p = 0xaa;
+    p = (unsigned char xdata*)0x02aa;
+    *p = 0x55;
+    p = (unsigned char xdata *)page;
+    *p = 0x30;
+    SPC_FNC = 0x00;
+    PCON = 0x01;
+}
+/*************************************************************************
+功能: IAP写一字节
+参数: addr写入地址 dat写入数据
+返回: void
+***************************************************************************/
+void BTL_IAPWriteByte(unsigned int addr, unsigned char dat)
+{
+    unsigned char xdata * p;
+    SPC_FNC = 0x01;
+    p = (unsigned char xdata*)0x0555;
+    *p = 0xaa;
+    p = (unsigned char xdata*)0x02aa;
+    *p = 0x55;
+    p = (unsigned char xdata*)0x0555;
+    *p = 0xa0;
+    p = (unsigned char xdata *)addr;
+    *p = dat;
+    SPC_FNC = 0x00;
+    PCON = 0x01;
+}
+/*************************************************************************
+功能: IAP写入多字节
+参数: addr 写入地址 date 数据指针  len 数据长度
+返回: void
+*备注：对flash进行写操作时，必须打开中断，否则会写入失败
+***************************************************************************/
+unsigned char BTL_IAPWrite(unsigned int addr,unsigned int line, unsigned char  xdata *date)
+{
+    unsigned int  i;
+    unsigned char xdata buff[512];
+    unsigned char code  *cp;
+    unsigned char INTIEEAtmp;
+    unsigned char EIEEAtmp;
+    unsigned char IEEAtmp;
+    bit EAtmp;
+    unsigned char CBANKEAtmp;
+    CBANKEAtmp = CBANK;
+
+    EAtmp = EA;
+    INTIEEAtmp = ExInt2IE;
+    EIEEAtmp = EIE;
+    IEEAtmp = IE;
+
+    ExInt2IE = 0x40;
+    IE = 0;
+    EIE = 0x01;
+    EA = 1;                                             // 任何其他中断发生都可能造成写入失败
+    CBANK = 1;
+    
+    addr = addr << 9;
+                                                        
+    for(i=0;i<0x200;i++)                                // 读取整页数据
+    {
+        cp = (unsigned char code *)(addr + i);  
+        buff[i] = (*cp);
+    }
+    
+    BTL_pageerase0(addr);
+
+    for (i=0;i<D_UPDATALEN;i++)                         // 改写需要修改的数据
+    {
+        buff[line+i]=*(date+i);
+    }
+    
+    for (i = 0; i < 0x200; i++)                          // 写入FLASH
+    {
+        BTL_IAPWriteByte(addr + i, buff[i]);
+    }
+
+
+    for (i = 0; i < 0x200; i++)                         // 如果写入的数据与收到的升级数据不同
+    {
+        cp = addr + i;
+        if ((*cp) != buff[i])
+        {
+            ExInt2IE = INTIEEAtmp;
+            EIE = EIEEAtmp;
+            IE = IEEAtmp;
+            EA = EAtmp;
+            CBANK = CBANKEAtmp;
+            return (D_FALSE);
+
+        }
+    }
+    ExInt2IE = INTIEEAtmp;
+    EIE = EIEEAtmp;
+    IE = IEEAtmp;
+    EA = EAtmp;
+    CBANK = CBANKEAtmp;
+
+    return (D_TRUE);
+}
+/*************************************************************************
+*函数功能：升级状态检测
+*参数：无
+*返回值：无
+*************************************************************************/
+unsigned char BTL_checkStatus(void )
+{
+#if 0
+    unsigned char xdata uartBuff[20];
+    unsigned char idata rtl;
+#endif
+    while (1)
+    {
+        if ((bootFlag[0] != 0x55) || (bootFlag[1] != 0x55))
+        {
+            return (0);
+        }
+        else
+        {
+            return (1);
+        }
+#if 0
+        if (D_RISET())
+        {
+            D_RICLR();
+            uartBuff[i] = SBUF5;
+            i++;
+            if (uartBuff[0] != '#')
+            {
+                i = 0;
+            }
+            if ((i >= 2) && (uartBuff[2] == '#'))
+            {
+                switch (uartBuff[1])
+                {
+                case 0x01:
+                    rtl = 1;
+                    break;
+                case 0x02:
+                    break;
+                case 0x03:
+                    break;
+                case 0x04:
+                    break;
+                default:
+                    rtl = 0;
+                    break;
+                }
+                return (rtl);
+
+            }
+        }
+#endif
+    }
+}
+/*************************************************************************
+*函数功能：升级进程
+*参数：无
+*返回值：无
+*************************************************************************/
+void BTL_Updata(void)
+{
+    unsigned char idata i = 0;
+    unsigned char idata ptr = 0;
+    unsigned int  idata base = 0;
+    unsigned char idata chk;
+    unsigned char xdata uartBuff[D_FRAMELEN];
+    unsigned int  idata time = 0;
+    unsigned char xdata *p = 0;
+    // 不擦出第一页的情况下，就不要再保存
+//  bootAddr[0] = *(unsigned char code *)(0x0000);      // should always equal 0x02
+//  bootAddr[1] = *(unsigned char code *)(0x0001);
+//  bootAddr[2] = *(unsigned char code *)(0x0002);      // 保存flash0x0000-0x0002三个字节的程序跳转信息
+
+    BTL_sendEcho(D_START);
+    BTL_memClr((unsigned char *)uartBuff, 20);                      // 清空数据缓冲
+    //BTL_memClr((unsigned char *)BTL_tempBuff, D_SECTORSIZE);
+    ptr = 0;
+    while (1)
+    {
+        D_CLRWDT();
+
+        BTL_timeCount(&time);                                       // 查询方式计时
+        if (D_RISET())                                              // 查询标志位读取通信数据
+        {
+            D_RICLR();
+            ExInt3IFG &= (~BIT3);
+            time = 0;                                               // 重置计时
+            uartBuff[ptr] = SBUF5;
+            ptr++;
+            if (uartBuff[D_FRAMESTARTLC] != '#')
+            {
+                ptr = 0;
+            }
+
+            if (ptr >= D_FRAMELEN )
+            {
+                if (uartBuff[D_FRAMESTOPLC] == '#')                   // 接收完整
+                {
+                    chk = 0;
+                    for (i = D_SECTORLC; i < D_CHKLC; i++)            // 计算校验码
+                    {
+                        chk += uartBuff[i];
+                    }
+
+                    if (chk == uartBuff[D_CHKLC])                           // hex校验正确
+                    {
+                        currentSector = uartBuff[D_SECTORLC];
+                        switch (uartBuff[D_FLAGLC])
+                        {
+                            case D_UPDATE:                                      // 接收升级数据
+                            {
+                                base = D_UPDATALEN * uartBuff[D_LINELC];
+                                
+                                if (BTL_IAPWrite(currentSector, base, &uartBuff[D_DATESTARTLC]))
+                                {
+                                    BTL_sendEcho(D_NEXTFRAME);                  // 向上位机请求发送下一帧数据
+                                }
+                                else
+                                {
+                                    BTL_sendEcho(D_REPEATFRAM);                  // 向上位机请求重发上一帧数据
+
+                                }
+                                ptr = 0;
+                                BTL_memClr((unsigned char *)uartBuff, D_FRAMELEN);
+                            }
+                            break;
+                            
+                            case D_ALLDATEDONE:                                     // 升级结束
+                            {
+                                BTL_sendEcho(D_UPDATADONE);
+                                D_NOP();
+                                #pragma ASM
+                                LJMP    ?C_START
+                                #pragma ENDASM
+                            }
+                            break;
+                            
+                            case D_STOPUPDATE:                                        // 强制结束升级,因为升级未完成，会每隔10秒请求一次升级
+                            {
+                                //BTL_sendEcho(D_START);
+                                BTL_memClr((unsigned char *)uartBuff, 20);             // 清空数据缓冲
+                                ptr = 0;
+                            }
+                            break;
+                            
+                            default:
+                            {
+                                ptr = 0;
+                                BTL_memClr((unsigned char *)uartBuff, D_FRAMELEN);
+                                BTL_sendEcho(D_REPEATFRAM);                             // 向上位机请求重发上一帧数据
+                            }
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        ptr = 0;
+                        BTL_memClr((unsigned char *)uartBuff, D_FRAMELEN);
+                        BTL_sendEcho(D_REPEATFRAM);                                     // 向上位机请求重发上一帧数据
+                    }
+                }
+                else
+                {
+                    ptr = 0;
+                    BTL_memClr((unsigned char *)uartBuff, D_FRAMELEN);
+                    BTL_sendEcho(D_REPEATFRAM);                                         // 向上位机请求重发上一帧数据
+                }
+            }
+        }
+        else
+        {
+            if (time >= D_TIMETH)
+            {
+                ptr = 0;
+                time = 0;
+                BTL_memClr((unsigned char *)uartBuff, D_FRAMELEN);
+                BTL_sendEcho(D_REPEATFRAM);                                             // 向上位机请求重发上一帧数据
+            }
+        }
+    }
+}
+/*************************************************************************
+*函数功能：计时函数
+*参数：tmth。计时变量指针
+*返回值：无
+*************************************************************************/
+void BTL_timeCount(unsigned int *tmTh)
+{
+    if (D_TFSET())                                                      // 查询定时器
+    {
+        TH0 = 0x2A;
+        TL0 = 0xAB;                                                     // 50ms定时
+        D_TFCLR();
+        (*tmTh)++;
+    }
+}
+/*************************************************************************
+*函数功能：返回帧
+*参数：content,返回帧内容
+*返回值：无
+*************************************************************************/
+void BTL_sendEcho(unsigned char content)
+{
+    unsigned char idata i;
+    unsigned char idata echo[4];
+    echo[0] = '#';
+    echo[3] = '#';
+    echo[1] = (unsigned char)(content / 10) + 0x30;
+    echo[2] = (unsigned char)(content % 10) + 0x30;
+    for (i = 0; echo[i] != '\0'; i++)
+    {
+        SBUF5 = echo[i];
+        while (!(SCON5 & 0x02));
+        SCON5 &= (~0x02);
+        ExInt3IFG &= (~0x04);
+    }
+}
+
+/*************************************************************************
+*函数功能：串口初始化
+*参数：无
+*返回值：无
+*************************************************************************/
+void BTL_uartInit(void )
+{
+    P1OE &= (~BIT4);
+    P1IE &= (~BIT4);
+    P1OE |= BIT3;
+    P1IE |= BIT3;
+    P13FS = 0x02;
+    P14FS = 0x02;
+    P1OD |= BIT4;
+
+    TCON5 = 0xA2;                               // SMOD=0,UART波特率加倍，关闭波特率发生器
+    SCON5 = 0x50;                               // 8位UART,UART4工作在模式1中,使能接收
+    TMOD5 = 0x20;                               // 波特率发生器模式：自动重装初值8为定时器（兼容定时器1）
+    TH51 = D_baud19200REG;
+    ExInt3IE &= (~(BIT3 + BIT2));               // 禁止接收与发送中断
+    EIE &= (~BIT1);                             // 禁止中断向量9
+}
+/*************************************************************************
+*函数功能：定时器初始化
+*参数：无
+*返回值：无
+*************************************************************************/
+void BTL_timerInit(void )
+{
+    CKCON &= ~(BIT3);                         // 系统时钟作为时钟源，12分频
+    TMOD  &= 0xF0;
+    TMOD  |= 0x01;                            // 定时器0工作在方式1:16位向上计数器
+    TH0 = 0x2A;
+    TL0 = 0xAB;                               // 50ms定时
+
+    TCON |= BIT4;                             // 开定时器0
+    IE &= (~BIT1);                            // 允许中断
+}
+/*************************************************************************
+*函数功能：系统时钟频率设置
+*参数：无
+*返回值：无
+*************************************************************************/
+unsigned char BTL_setSysClock(unsigned char PLLClk)
+{
+    unsigned char  i;
+    i = 0;
+    FSC = 0;
+    FWC = 0;
+    MCUFRQ = 0;                       //  设置时钟1的时钟源OSC
+    while ((MCUFRQ))
+    {
+        i++;
+        if (i > 20)
+        {
+            return (0);                // 在一定时间内没有锁定
+        }
+    }
+    MEAFRQ = (0);                      // 设置时钟2的时钟源OSC
+    while ((MEAFRQ))
+    {
+        i++;
+        if (i > 20)
+        {
+            return (0);                // 在一定时间内没有锁定
+        }
+    }
+    for (i = 0; i < 5; i++);
+    CtrlCLK = 0xc0;                 // 开始PLL电路
+    CtrlPLL = 0;                    // 800K pll 800k, DSP 800K,adc 200k,并切换到800kPLL
+    while (!(PLLLCK & 0x01))         // 判断PLL时钟是否锁定
+    {
+        i++;
+        if (i > 50)
+        {
+            return (0);               // 在一定时间内没有锁定
+        }
+    }
+
+    MCUFRQ = 1;                    // 设置时钟1的时钟源PLL
+    i = 0;
+    while (!(MCUFRQ))
+    {
+        i++;
+        if (i > 20)
+        {
+            return (0);             // 在一定时间内没有锁定
+        }
+    }
+    MEAFRQ = 1;                   // 设置时钟2的时钟源PLL
+    i = 0;
+    while (!(MEAFRQ))
+    {
+        i++;
+        if (i > 20)
+        {
+            return (0);            // 在一定时间内没有锁定
+        }
+    }
+//  PMG=0;                         // 开启计量时钟
+    switch (PLLClk)
+    {
+    case 0:                    // pll 800k, DSP 800K,adc 200k,并切换到800kPLL
+    {
+        CtrlCLK = 0xc0;
+        CtrlPLL = 0x00;
+    }
+    break;
+
+    case 1:                   // pll 1.6M, DSP 1.6M,adc 400k,并切换到1.6MPLL
+    {
+        CtrlCLK = 0xd5;
+        CtrlPLL = 0x00;
+    }
+    break;
+
+    case 2:                   // pll 3.2M, DSP 3.2M,adc 800k,并切换到3.2MPLL
+    {
+        CtrlCLK = 0xea;
+        CtrlPLL = 0x00;
+    }
+    break;
+
+    case 3:                  // pll 6.4M, DSP 3.2M,adc 800K,并切换到6.4MPLL
+    {
+        CtrlCLK = 0xeb;
+        CtrlPLL = 0x00;
+    }
+    break;
+
+    case 4:                  // pll 13M， DSP 3.2M,adc 800K,并切换到13MPLL
+    {
+        CtrlCLK = 0xeb;
+        CtrlPLL = 0x40;
+    }
+    break;
+
+    case 5:                  // pll 26M， DSP 3.2M,adc 800K,并切换到26MPLL
+    {
+        CtrlCLK = 0xeb;
+        CtrlPLL = 0xc0;
+    }
+    break;
+
+    default:                // pll 3.2M, DSP 3.2M,adc 800k,并切换到3.2MPLL
+    {
+        CtrlCLK = 0xea;
+        CtrlPLL = 0x00;
+    }
+    break;
+    }
+
+
+    while (!(PLLLCK & 0x01))
+    {
+        i++;
+        if (i > 50)
+        {
+            return (0);        // 在一定时间内没有锁定
+        }
+    }
+    MCUFRQ = 1;
+    i = 0;
+    while (!(MCUFRQ))
+    {
+        i++;
+        if (i > 20)
+        {
+            return (0);       // 在一定时间内没有锁定
+        }
+    }
+    MEAFRQ = 1;
+    i = 0;
+    while (!(MEAFRQ))
+    {
+        i++;
+        if (i > 20)
+        {
+            return (0);      // 在一定时间内没有锁定
+        }
+    }
+
+//  PMG=0;                  // 开启计量时钟
+    CtrlCry0 = 0;           // 调整启振波形
+    CtrlCry1 = 3;
+    CtrlCry2 |= 0x20;         // 使能起振电路停振复位
+
+
+    CtrlBGP = 0x02;         // 改善高低温误差+10PPM
+    CtrlM = 0x01;           // 禁止M通道去直
+    if (CtrlCLK == 0xc0)       // 降频计量时 降低功耗
+    {
+        CtrlBGP |= 0xc0;    // 偏置电流减小66%
+    }
+    else
+    {
+        CtrlBGP &= (~(0xc0));
+    }
+
+    return (1);
+}
---
+++ bootloader.h
@@ -0,0 +1,116 @@
+#ifndef _BOOTLOADER_H
+#define _BOOTLOAER_H
+#include "RWMEAReg.h"
+#include "V9811.h"
+#include <INTRINS.h>
+/*波特率寄存器的值*/
+#define D_baud115200REG 0xF9
+#define D_baud19200REG  0xD5
+#define D_baud9600REG   0xAB
+#define D_baud4800REG   0x55
+#define D_baud2400REG   0xE4
+#define D_baud1200REG   0xC7
+#define D_baud600REG    0x8E
+#define D_baud300REG    0x1C
+
+// 宏
+#ifndef NULL
+    #define NULL            0
+#endif
+
+#define BIT0            0x01
+#define BIT1            0x02
+#define BIT2            0x04
+#define BIT3            0x08
+#define BIT4            0x10
+#define BIT5            0x20
+#define BIT6            0x40
+#define BIT7            0x80
+
+#define D_FALSE     	0
+#define D_TRUE			1
+#define D_ABSOLUTE  	1
+#define D_RELATIVE		0
+#define RI_             0x01
+#define TI_             0x02
+#define D_DISINT()      (EA = 0)
+#define D_CLRWDT()      do {wdt0=0xA5;wdt1=0x5A; wdt0=0xA5;wdt1=0x5A;} while(0)
+#define D_RISET()       (SCON5 & RI_)
+#define D_RICLR()       do {SCON5 &= (~RI_); ExInt3IFG &= (~BIT3);}while(0)
+#define D_TFSET()       (TCON & BIT5)
+#define D_TFCLR()       (TCON &= (~BIT5))
+
+// echo代码
+#define D_STOP         	0x00    // 结束升级
+#define D_START         0x01    // 开始升级，此时上位机可以发送第一帧数据
+
+#define D_NEXTFRAME     0x02    // 请求下一帧数据
+#define D_NEXTSECTOR    0x03    // 请求下一扇区数据
+
+#define D_UPDATADONE    0x04    // 升级完成
+#define D_UPDATAFAIL    0x05    // 升级失败
+
+#define D_REPEATFRAM    0x06    // 重复请求上一帧数据
+#define D_REPEATSECTOR  0x07    // 重复请求上一扇区数据
+#define D_BTL           0x08    // bootloader标志
+
+#define D_NOP()         _nop_()
+
+#define D_V98XX         0
+#define D_SH7012        1
+
+// 全局设置
+#define D_FLAGADDR      0x0FF0                          // xdata区，用于作为升级的全局标志
+#define D_MCU           D_V98XX
+#define D_UPDATALEN  	16							    // 每帧的升级数据数据长度
+#define D_FRAMELEN      (D_UPDATALEN+6)                 // 每帧实际数据长度()
+#define D_ADRESSMODE    D_ABSOLUTE					    // 寻址模式
+#define D_READCHECK		D_FALSE						    // 读出比对
+#define D_TIMETH        (200)                            // *50ms
+
+// 关键标识数组下标
+#define D_FRAMESTARTLC  0								// 帧头
+#define D_FRAMESTOPLC	(D_FRAMELEN-1)					// 帧尾
+#define D_SECTORLC		1								// 扇区号
+#define D_LINELC		2								// 行号
+#define D_FLAGLC		3								// 标志位(用于升级控制)
+#define D_DATESTARTLC	4								// 数据开始
+#define D_CHKLC			(D_FRAMELEN-2)					// 校验位
+
+// 升级控制标识
+#define D_UPDATE 		0x00
+#define D_SECTORDONE	0x01
+#define D_ALLDATEDONE	0x02
+#define D_STOPUPDATE	0x03
+// note* bootloader位于flash中的最后10k_(10或20页)(0xD800-0xFFFF),其中0xFE00页用于保存用户程序的入口地址
+#if (D_MCU == D_V98XX)
+    #define D_SECTORSUM		0x6c						// 128-20页
+    #define D_SECTORSIZE	0x200 						// 每页大小，V98xx是512个字节 中颖7012是1024个字节
+#endif
+#if (D_MCU == D_SH7012)
+    #define D_SECTORSUM		0x36 						// 64-10页
+    #define D_SECTORSIZE/	0x400 						// 每页大小，V98xx是512个字节 中颖7012是1024个字节
+#endif
+
+#define D_FRAMENUM		(D_SECTORSIZE/D_UPDATALEN)	    // 传输一页数据需要的总帧数
+
+//// 用户主函数入口地址
+//typedef void (code*USERAPP)(void);                      // 函数指针
+//#define D_MAINBASEADDR     (0x0600)
+
+// 函数申明
+
+
+extern unsigned char BTL_checkStatus(void );
+extern void BTL_timeCount(unsigned int *tmTh);
+extern void BTL_Updata(void);
+extern void BTL_flashErase(unsigned char sector);
+extern void BTL_memCpy(unsigned char *des, unsigned char const *src, unsigned int len);
+extern void BTL_memClr(unsigned char *des,unsigned int len);
+extern unsigned char BTL_IAPWrite(unsigned int addr,unsigned int line, unsigned char  xdata *date);
+extern void BTL_flashInit(void);
+extern void BTL_uartInit(void);
+extern void BTL_timerInit(void);
+extern void BTL_sendEcho(unsigned char content);
+extern unsigned char BTL_setSysClock(unsigned char PLLClk);
+#endif
